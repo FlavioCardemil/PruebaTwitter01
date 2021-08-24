e5f696ebeb630bb7efa9cf6b4d1cec57aa5ea3bf
Flavio <flavio.cardemil@gmail.com>
2021-08-24 17:25:13 -0400

    Likes on

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,14 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def to_like
-    tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
     new_like = Like.create(tweet: @tweet, user: current_user)
     redirect_to root_path
   end
 
   def to_dislike
-    tweet = Tweet.find(params[:id])
-    like = tweet.likes.find_by(user: current_user)
+    @tweet = Tweet.find(params[:id])
+    like = @tweet.likes.find_by(user: current_user)
     like.destroy
     redirect_to root_path
   end
